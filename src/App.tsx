ad0d9556bb8d410181b72e43b3d35911c4ea4437
前端开发部 - 陈骥 <chenji@jiguang.cn>
2020-11-08 22:14:54 +0800

    feat: button组件添加样式

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <Button> Hello World </Button>
-       <Button disabled> Hello World </Button>
-       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello World </Button>
-       <Button btnType={ButtonType.Link} href="https://www.baidu.com"> Hello World </Button>
+       <Button> Hello </Button>
+       <Button disabled> disabled Button </Button>
+       <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Primary Large</Button>
+       <Button btnType={ButtonType.Primary} size={ButtonSize.Small}> Primary Small</Button>
+       <Button btnType={ButtonType.Link} href="https://www.baidu.com"> Link </Button>
+       <Button btnType={ButtonType.Link} href="https://www.baidu.com" disabled> Link </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
