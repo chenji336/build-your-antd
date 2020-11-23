8f9481b9b8374626dc7ffd0b5f200ed958f01093
前端开发部 - 陈骥 <chenji@jiguang.cn>
2020-11-23 09:41:41 +0800

    feat: Transition 组件添加 wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
 import Menu from './components/Menu/menu'
 import MenuItem from './components/Menu/menuItem'
@@ -8,10 +8,12 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import Icon from './components/Icon/icon';
+import Transition from './components/Transition/transition';
 
 library.add(fas) // 这样就不需要一个一个引入了（fas 是 IconPack)
 
 function App() {
+  const [show, setShow] = useState(false)
   return (
     <div className="App">
       <header className="App-header">
@@ -45,17 +47,29 @@ function App() {
           </SubMenu>
         </Menu>
 
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button size={ButtonSize.Large} onClick={() => setShow(!show)}>toggle</Button>
+        <Transition in={show} timeout={300} animation="zoom-in-left">
+         <div>
+          <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+         </div>
+        </Transition>
+        {/* 
+          Button 动画无效，因为 Button 自己也有 transition，覆盖掉了 
+          添加 wrapper 属性可以解决这个问题
+        */}
+        <Transition in={show} timeout={300} animation="zoom-in-left" wrapper>
+          <Button btnType={ButtonType.Primary}>primary Button</Button>
+        </Transition>
       </header>
     </div>
   );
--- src/components/Transition/transition.tsx
+++ src/components/Transition/transition.tsx
@@ -5,7 +5,8 @@ import { CSSTransitionProps } from 'react-transition-group/CSSTransition'
 type AnimationName = 'zoom-in-top' | 'zoom-in-left' | 'zoom-in-bottom' | 'zoom-in-right'
 
 type TransitionProps = CSSTransitionProps & {
-  animation?: AnimationName
+  animation?: AnimationName,
+  wrapper?: boolean, // 最外一层添加一层父元素，防止 transition 被覆盖
 }
 
 const Transition: React.FC<TransitionProps> =(props) => {
@@ -13,6 +14,7 @@ const Transition: React.FC<TransitionProps> =(props) => {
     children,
     animation,
     classNames,
+    wrapper,
     ...restProps
   } = props
   
@@ -22,18 +24,25 @@ const Transition: React.FC<TransitionProps> =(props) => {
   // fix: https://github.com/reactjs/react-transition-group/issues/668
   // https://github.com/reactjs/react-transition-group/blob/1fd4a65ac45edd2aea3dec18eeb8b9c07c7eb93f/CHANGELOG.md#features
   const nodeRef = useRef(null)
-  let childrenEnhance: React.FunctionComponentElement<any> = <div>只是个占位符号，避免报错</div>
-  React.Children.map(children, (child, index) => {
-    const childElement = child as React.FunctionComponentElement<any>
-    const cloneChildElement = React.cloneElement(childElement, {
-      ref: nodeRef
+  let childrenEnhance: any
+  if (wrapper) {
+    childrenEnhance = <div ref={nodeRef}>{children}</div>
+  } else {
+    React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<any>
+      const cloneChildElement = React.cloneElement(childElement, {
+        ref: nodeRef
+      })
+      // CSSTransition 必须包含一个根元素，所以这个定义会有一个元素
+      if (index === 0) {
+        childrenEnhance = cloneChildElement
+      } else {
+        childrenEnhance = children // 如果元素超过 1 个，则把 children 赋值给 childrenEnhance（使用 CSSTransition 自己的报错）
+      }
+      return cloneChildElement
     })
-    // CSSTransition 必须包含一个根元素，所以这个定义会有一个元素
-    if (index === 0) {
-      childrenEnhance = cloneChildElement
-    }
-    return cloneChildElement
-  })
+  }
+
 
    /** end: 给 children 添加 ref */
 
